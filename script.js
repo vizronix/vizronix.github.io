ab8bef2d733dfb1fcf4f3e78a55eea27098d2883
Vizronix <ajwadpk2008@gmail.com>
2025-06-08 22:27:15 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
+function toggleSidebar() {
+    var sidebar = document.getElementById("sidebar");
+    var closeBtn = document.querySelector(".close-btn");
+    var toggleBtn = document.querySelector(".toggle-btn");
 
+    if (sidebar.style.left === "1500px" || sidebar.style.left === "") {
+        sidebar.style.left = "0px";
+        closeBtn.style.display = "block";
+        toggleBtn.style.display = "none";
+    } else {
+        sidebar.style.left = "1500px";
+        closeBtn.style.display = "none";
+        toggleBtn.style.display = "block";
+    }
+}
